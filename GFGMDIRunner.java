4265eab046d11891bf310e17bfa200ccf39b5ef8
Nitin Jayant <thenitinjayant@gmail.com>
2021-01-06 15:02:50 +0530

    runner added

---
+++ GFGMDIRunner.java
@@ -0,0 +1,10 @@
+package GFG_MDI_Questions;
+
+import GFG_MDI_Questions.MD_Arrays.*;
+
+public class GFGMDIRunner {
+    public void run(){
+        MissingNumberInArray missingNumberInArray = new MissingNumberInArray();
+        missingNumberInArray.mainMissingNumberInArray();
+    }
+}
